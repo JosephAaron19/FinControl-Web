ed8310bf875fd7319c0ab07042b5d0987fec418e
Tu Nombre <crazykeyworker19@gmail.com>
2026-05-08 17:56:01 -0500

    produccion ajustes

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -17,7 +17,7 @@ const Login = () => {
     setError(null);
     
     try {
-      const response = await fetch('http://localhost:8000/api/auth/login/', {
+      const response = await fetch('https://apifincontrol.finatech.com.pe/api', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
